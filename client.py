8e90b900beabd2c6652225694254b2d1ff33125d
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-06-17 01:58:56 +0530

    da.1

--- client.py
+++ client.py
@@ -1,20 +1,35 @@
 import socket
 
+
 def client_program():
     host = socket.gethostname()
-    port = 8000
+    port = 6000
 
     client_socket = socket.socket()
-
     client_socket.connect((host, port))
 
-    message = input(" -> ")
+    # Step 1: Send a string to the server
+    message = input("\nEnter a string to send to the server: ")
+    client_socket.send(message.encode())
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024).decode()
-        print("Received from server : " + data)
-        message = input(" -> ")
+    # Step 2: Read 'n' and 'n' integers from the user
+    n = int(input("Enter the number of integers to send: "))
+    client_socket.send(str(n).encode())
+
+    integers = []
+    for i in range(n):
+        num = int(input("\nEnter an integer: "))
+        integers.append(num)
+        client_socket.send(str(num).encode())
+
+    # Step 3: Receive PN and NN from the server
+    pn = client_socket.recv(1024).decode()
+    print("\nReceived PN from server:", pn)
+    client_socket.send(b"Acknowledged")  # Send acknowledgment to the server
+
+    nn = client_socket.recv(1024).decode()
+    print("Received NN from server:", nn, "\n")
+
 
     client_socket.close()
 
--- server.py
+++ server.py
@@ -3,22 +3,39 @@ import socket
 
 def server_program():
     host = socket.gethostname()
-    port = 8000
+    port = 6000
 
     server_socket = socket.socket()
-
     server_socket.bind((host, port))
 
     server_socket.listen(1)
     conn, address = server_socket.accept()
-    print("Connection from : " + str(address))
-    while True: 
-        data = conn.recv(1024).decode()
-        if not data:
-            break
-        print("from connected user : " + str(data))
-        data = input (' -> ')
-        conn.send(data.encode())
+    print("\nConnection from: " + str(address))
+
+    # Step 1: Receive the string from the client
+    data = conn.recv(1024).decode()
+    print("\nReceived string from client: " + str(data))
+
+    # Step 2: Receive 'n' number of integers from the client
+    n = int(conn.recv(1024).decode())
+    print("The number of integers from client : {}".format(n))
+
+    integers = []
+    for i in range(n):
+        num = int(conn.recv(1024).decode())
+        integers.append(num)
+        print("\nReceived integer from client: {}".format(num))
+
+    # Step 3: Compute PN and NN
+    pn = sum([x for x in integers if x > 0])
+    nn = sum([x for x in integers if x < 0])
+    print("\nComputed PN: {}, NN: {}".format(pn, nn))
+    print("\n")
+
+    # Step 4: Send PN and NN to the client
+    conn.send(str(pn).encode())
+    conn.recv(1024)  # Receive acknowledgment from the client
+    conn.send(str(nn).encode())
 
     conn.close()
 
